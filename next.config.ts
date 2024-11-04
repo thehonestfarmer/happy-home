29de2299234a21cd8d0adf7ea69224da46995855
thehonestfarmer <thehonestfarmer@proton.me>
2024-11-03 18:58:20 -0800

    next

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   experimental: {
     scrollRestoration: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
