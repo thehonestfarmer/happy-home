802d53e0f448cd578d2eeaf503e90c3263ff165a
thehonestfarmer <thehonestfarmer@proton.me>
2024-12-10 15:30:27 -0800

    ye ye

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,24 +2,5 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 
 export async function GET() {
-  await init();
-
   return NextResponse.json({ data: 123 });
-}
-
-async function init() {
-  console.log("... running init");
-  const json = {};
-
-  try {
-    // Write the JSON data to a file
-    await fs.promises.writeFile(
-      "output.json",
-      JSON.stringify(json, null, 2),
-      "utf-8",
-    );
-    console.log("Data successfully written to output.json");
-  } catch (error) {
-    console.error("Error writing to file:", error);
-  }
 }
