fd461a2ec4c505bd2b5a5edee24916f66e422155
thehonestfarmer <thehonestfarmer@proton.me>
2025-03-28 16:18:00 +0700

    cron

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // This applies to /api/cron and all paths under it
+        source: '/api/cron/:path*',
+        headers: [
+          {
+            key: 'x-vercel-cron',
+            // Use the environment variable
+            value: process.env.VERCEL_CRON_SECRET || ''
+          }
+        ]
+      }
+    ]
+  },
   serverExternalPackages: ['bull'],
   experimental: {
     scrollRestoration: true,
